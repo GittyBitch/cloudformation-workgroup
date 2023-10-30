b2a475291a4071f710ffb35594b35f1cfc10bf98
Marc E. <marc.ehrlich@docc.techstarter.de>
2023-10-30 16:34:03 +0100

    userdata script written

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+#python3 zu installieren (yum)
+#StarChaser Projekt runter zu laden (github)
+#installieren der Pakete aus requirements.txt
+#python3 Script ausführen
 
-echo "Dies ist ein Userdata Script"
+#url zum download 
+url = http
+
+# haupt datei
+startdatei = main.py
+
+# alle updates instalieren
+yum update -y
+
+#python3 installieren 
+yum install -y python3
+
+#starchaser runterladen
+wget $url
+
+#datei entpacken
+pip install -r requirements.txt
+
+#script mit python ausführen
+python3 $startdatei
